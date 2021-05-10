fb9fbe30acef3501302235be00a7c1c761e26533
asapp-coding <78867126+asapp-coding@users.noreply.github.com>
2021-05-10 08:21:59 -0400

    Add files via upload

---
+++ js.js
@@ -0,0 +1,16 @@
+var navLinks = document.getElementById("navLinks");
+
+        function showMenu() {
+            navLinks.style.right = "0";
+        }
+        function hideMenu() {
+            navLinks.style.right = "-200px"
+        }
+
+
+
+let backgroundColor = document.getElementById("cta-color");
+
+function changeColor() {
+    let colors = []
+}
